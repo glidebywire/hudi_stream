095dacb24e73050acd4b3c46a6871219a9e24251
jupiterb76p <jupiterb76p@gmail.com>
2025-09-17 15:25:51 +0200

    the further we got

--- Hudi-Streamer-with-Apache-Airflow/airflow/dags/dags_readminio_data.py
+++ Hudi-Streamer-with-Apache-Airflow/airflow/dags/dags_readminio_data.py
@@ -6,7 +6,7 @@ with DAG(
   dag_id="minio_read_test",
   start_date=datetime.datetime(2025, 7, 14),
   schedule="@once",
-  catchup=False
+  catchup=False,
 ):
   BashOperator(
     task_id="minio_read_test_task",
@@ -16,9 +16,11 @@ spark-submit \
   --master spark://spark:7077 \
   --deploy-mode client \
   --verbose \
-  --jars /opt/spark/extra-jars/hudi-spark3.5-bundle_2.12-1.0.2.jar,/opt/spark/extra-jars/hadoop-aws-3.3.4.jar,/opt/spark/extra-jars/aws-java-sdk-bundle-1.12.262.jar,/opt/spark/extra-jars/postgresql-42.7.7.jar \
-  --conf 'spark.driver.extraJavaOptions=-Dcom.amazonaws.sdk.disableClockSkewAdjustment=true -Duser.timezone=UTC -Dorg.apache.xerces.features.disallow-doctype-decl=false' \
-  --conf 'spark.executor.extraJavaOptions=-Dcom.amazonaws.sdk.disableClockSkewAdjustment=true -Duser.timezone=UTC -Dorg.apache.xerces.features.disallow-doctype-decl=false' \
+  --jars /opt/airflow/external-jars/hudi-spark3.5-bundle_2.12-1.0.2.jar,/opt/airflow/external-jars/hadoop-aws-3.3.4.jar,/opt/airflow/external-jars/aws-java-sdk-bundle-1.12.262.jar \
+  --conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer' \
+  --conf 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.hudi.catalog.HoodieCatalog' \
+  --conf 'spark.sql.extensions=org.apache.spark.sql.hudi.HoodieSparkSessionExtension' \
+  --conf 'spark.kryo.registrator=org.apache.spark.HoodieSparkKryoRegistrar' \
   /opt/airflow/jobs/jobs_readminio.py
 """
   )
