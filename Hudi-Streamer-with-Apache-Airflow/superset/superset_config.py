164a177901ef670c6c4e19e2c6fe984ed1f62c65
jupiterb76p <jupiterb76p@gmail.com>
2025-09-18 14:49:22 +0200

    hudi hive sync airflow trino OK except superset

---
+++ Hudi-Streamer-with-Apache-Airflow/superset/superset_config.py
@@ -0,0 +1,6 @@
+ENABLE_PROXY_FIX = True
+SECRET_KEY = "MyVerySecretKey"
+PREVENT_UNSAFE_DB_CONNECTIONS = False
+TALISMAN_ENABLED = False
+
+SQLALCHEMY_DATABASE_URI = "postgresql://superset:superset@database:5432/superset"
